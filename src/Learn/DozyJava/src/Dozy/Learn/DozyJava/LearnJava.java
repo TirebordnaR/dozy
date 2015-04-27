cefc4ef42e5ae7ed7f1ca66328f64c43a7ed5f54
zapline <278998871@qq.com>
2015-04-27 11:21:28 +0800

    + DozyJava开坑

---
+++ src/Learn/DozyJava/src/Dozy/Learn/DozyJava/LearnJava.java
@@ -0,0 +1,12 @@
+package Dozy.Learn.DozyJava;
+
+public class LearnJava {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hey java, you're good. 才怪咧！");
+    }
+
+}
